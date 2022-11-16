b3be7d65292c17cfa73b1d4df95107f86104e0d4
TinuBose <117371766+TinuBose@users.noreply.github.com>
2022-11-16 16:29:45 +0000

    Add files via upload

---
+++ positve_negetive.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter a number:");
+scanf("%d",&a);
+if(a>0)
+printf("\n%d is positive\n",a);
+else if(a<0)
+printf("\n%d is negetive\n",a);
+else
+printf("\nzero\n");
+}
+
